67232a659ba9828644134d940616cc23f0075674
Seongil-Shin <callmeshin75@gmail.com>
2022-10-01 23:15:01 +0900

    basic - 스프링 빈까지

--- spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,14 +1,17 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemeoryMemberRepository;
 
-public class OrderServiceImpl implements  OrderService{
-    private final MemberRepository memberRepository = new MemeoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -18,3 +21,5 @@ public class OrderServiceImpl implements  OrderService{
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 }
+
+
